566e7130f7468bb64cbe3c4964fedb51e05deb37
Matthew Ruvalcaba <mattr@enjoykitchen.mx>
2021-03-01 09:42:11 -0800

    juego

---
+++ triangle.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'ruby2d'
+
+# Set es un metodo del que pasamos un Hash, con un simbolo "tittle y background."
+# Hash es un valor que realacion con simbolo (key) con un string, en si relacion valores,
+# como Empleado -=> sueldo
+
+set title: 'Hello Triangle', background: 'random'
+
+show
